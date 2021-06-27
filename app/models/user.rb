f0c8c6ceac2f39acb30e54809e9527666de19ccc
Elijah Samuels <elijahsamuels@gmail.com>
2021-06-27 17:19:24 -0600

    mid point save

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
 
 	validates_uniqueness_of :username
 
+	def self.generate
+		adjectives = ['Ancient', 'Creative', 'Immortal', 'Questionable', 'Unruly']
+		nouns = ['Puppy', 'Cat', 'Parrot', 'Fish', 'Turtle']
+		number = rand.to_s[2..5]
+		username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+		create(username: username)
+	end
+
 end
